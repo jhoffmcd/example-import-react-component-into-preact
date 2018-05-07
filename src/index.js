71554ce883bf57adc54410342f98097b155d262c
Jesse Hoffman <jhoffmcd@gmail.com>
2018-05-07 12:17:14 -0700

    Updated example

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import { h, render } from 'preact';
+import { MyModule } from 'example-react-component-export'; // Linked locally via "npm link"
 
+// This component renders correctly.
 const TestComp = () => 
     <div>
         Hi, I am a component (Preact)
     </div>
 
+// MyModule throws "Module not found: Error: Can't resolve 'react' in..."
 render(
-    <TestComp />,
+    <div>
+        <MyModule />,
+        <TestComp />
+    </div>,
     document.body
 );
